31fb012dbdd8e1adb2f2f93ec6d478ee51f6340d
Benoit TRAVERS <benoit.travers.externe@foncia.com>
2025-12-19 17:57:45 +0100

    fix: vitest instance

--- packages/testing/src/extension.ts
+++ packages/testing/src/extension.ts
@@ -7,18 +7,18 @@ export const it = vitestIt.extend<{
 }>({
   // oxlint-disable-next-line no-empty-pattern
   workerConnection: async ({}, use) => {
-    const connection = await connect(
-      `amqp://guest:guest@${inject("__TESTCONTAINERS_RABBITMQ_IP__")}:${inject("__TESTCONTAINERS_RABBITMQ_PORT_5672__")}`,
-    );
+    const connection = await createAmqpConnection();
     await use(connection);
-    await connection.close();
   },
   // oxlint-disable-next-line no-empty-pattern
   clientConnection: async ({}, use) => {
-    const connection = await connect(
-      `amqp://guest:guest@${inject("__TESTCONTAINERS_RABBITMQ_IP__")}:${inject("__TESTCONTAINERS_RABBITMQ_PORT_5672__")}`,
-    );
+    const connection = await createAmqpConnection();
     await use(connection);
-    await connection.close();
   },
 });
+
+function createAmqpConnection(): Promise<ChannelModel> {
+  return connect(
+    `amqp://guest:guest@${inject("__TESTCONTAINERS_RABBITMQ_IP__")}:${inject("__TESTCONTAINERS_RABBITMQ_PORT_5672__")}`,
+  );
+}
