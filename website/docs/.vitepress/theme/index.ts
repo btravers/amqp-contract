5f53739410375d264e3dbce33217744219f3e9cc
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2025-12-24 20:35:26 +0100

    fix: website theme

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -1,4 +1,6 @@
-import "./custom.css";
 import DefaultTheme from "vitepress/theme";
+
+// eslint-disable-next-line sort-imports this CSS import must come after DefaultTheme so that it can override
+import "./custom.css";
 
 export default DefaultTheme;
