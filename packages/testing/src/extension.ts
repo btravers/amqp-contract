4cfe3e8ffd7c73d53a80fe89257bfb82447d9271
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2025-12-26 14:03:01 +0100

    refactor: integration test context

--- packages/testing/src/extension.ts
+++ packages/testing/src/extension.ts
@@ -1,11 +1,93 @@
+import amqpLib, { type Channel, type ChannelModel } from "amqplib";
 import { inject, it as vitestIt } from "vitest";
+import { randomUUID } from "node:crypto";
 
 export const it = vitestIt.extend<{
+  vhost: string;
   amqpConnectionUrl: string;
+  amqpConnection: ChannelModel;
+  amqpChannel: Channel;
+  publishMessage: (exchange: string, routingKey: string, content: unknown) => void;
+  initConsumer: (
+    exchange: string,
+    routingKey: string,
+    nbEvents?: number,
+  ) => Promise<amqpLib.ConsumeMessage[]>;
 }>({
   // oxlint-disable-next-line no-empty-pattern
-  amqpConnectionUrl: async ({}, use) => {
-    const url = `amqp://guest:guest@${inject("__TESTCONTAINERS_RABBITMQ_IP__")}:${inject("__TESTCONTAINERS_RABBITMQ_PORT_5672__")}`;
+  vhost: async ({}, use) => {
+    const vhost = await createVhost();
+    await use(vhost);
+  },
+  amqpConnectionUrl: async ({ vhost }, use) => {
+    const url = `amqp://guest:guest@${inject("__TESTCONTAINERS_RABBITMQ_IP__")}:${inject("__TESTCONTAINERS_RABBITMQ_PORT_5672__")}/${vhost}`;
     await use(url);
   },
+  amqpConnection: async ({ amqpConnectionUrl }, use) => {
+    const connection = await amqpLib.connect(amqpConnectionUrl);
+    await use(connection);
+    await connection.close();
+  },
+  amqpChannel: async ({ amqpConnection }, use) => {
+    const channel = await amqpConnection.createChannel();
+    await use(channel);
+    await channel.close();
+  },
+  publishMessage: async ({ amqpChannel }, use) => {
+    function publishMessage(exchange: string, routingKey: string, content: unknown): void {
+      amqpChannel.publish(exchange, routingKey, Buffer.from(JSON.stringify(content)));
+    }
+    await use(publishMessage);
+  },
+  initConsumer: async ({ amqpChannel }, use) => {
+    async function initConsumer(
+      exchange: string,
+      routingKey: string,
+      nbEvents = 1,
+    ): Promise<amqpLib.ConsumeMessage[]> {
+      const queue = randomUUID();
+
+      await amqpChannel.assertQueue(queue);
+      await amqpChannel.bindQueue(queue, exchange, routingKey);
+
+      return new Promise((resolve) => {
+        const messages: amqpLib.ConsumeMessage[] = [];
+        amqpChannel.consume(
+          queue,
+          (msg) => {
+            if (msg) {
+              messages.push(msg);
+              if (messages.length >= nbEvents) {
+                resolve(messages);
+              }
+            }
+          },
+          { noAck: true },
+        );
+      });
+    }
+    await use(initConsumer);
+  },
 });
+
+async function createVhost() {
+  const namespace = randomUUID();
+
+  const vhostResponse = await fetch(
+    `http://${inject("__TESTCONTAINERS_RABBITMQ_IP__")}:${inject("__TESTCONTAINERS_RABBITMQ_PORT_15672__")}/api/vhosts/${namespace}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Basic ${btoa("guest:guest")}`,
+      },
+    },
+  );
+
+  if (vhostResponse.status !== 201) {
+    throw new Error(`Failed to create vhost: ${vhostResponse.status}`, {
+      cause: vhostResponse,
+    });
+  }
+
+  return namespace;
+}
