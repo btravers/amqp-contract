0d6187ad432850512c37d36742ae061835ed17e5
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2025-12-27 20:03:38 +0100

    fix: knip

--- samples/basic-order-processing-client-nestjs/src/use-cases/update-order-status.use-case.ts
+++ samples/basic-order-processing-client-nestjs/src/use-cases/update-order-status.use-case.ts
@@ -1,6 +1,6 @@
 import { Future, Result } from "@swan-io/boxed";
 import { Injectable, Logger } from "@nestjs/common";
-import type { MessageValidationError, TechnicalError } from "@amqp-contract/client";
+import type { MessageValidationError, TechnicalError } from "@amqp-contract/client-nestjs";
 import { AmqpClientService } from "@amqp-contract/client-nestjs";
 import type { orderContract } from "@amqp-contract-samples/basic-order-processing-contract";
 
