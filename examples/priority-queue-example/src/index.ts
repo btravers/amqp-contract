03e53fd7c3f5486191bcc940678f3ea48655286f
Benoit TRAVERS <benoit.travers.externe@foncia.com>
2026-01-20 00:07:11 +0100

    chore: example entrypoint

---
+++ examples/priority-queue-example/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Priority Queue Example
+ *
+ * This example demonstrates priority queue processing with amqp-contract.
+ *
+ * Run the client (publisher):
+ *   pnpm start:client
+ *
+ * Run the worker (consumer):
+ *   pnpm start:worker
+ */
+
+export { priorityQueueContract } from "./contract.js";
